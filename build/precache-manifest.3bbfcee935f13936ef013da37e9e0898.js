b0a9077c4edbbcea2aa8c745b1f19c273509035e
dragin96 <alexxx55509@mail.ru>
2020-08-28 10:51:54 +0300

    init github page

---
+++ build/precache-manifest.3bbfcee935f13936ef013da37e9e0898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc561378c6aea5aa79d9534f07dee641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845c940604955b7b22ff",
+    "url": "/static/css/main.2c891d92.chunk.css"
+  },
+  {
+    "revision": "b35b5e16b8d2fe070673",
+    "url": "/static/js/2.3d4a6a32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3d4a6a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845c940604955b7b22ff",
+    "url": "/static/js/main.a250c209.chunk.js"
+  },
+  {
+    "revision": "9c38578d78ee052cc19c",
+    "url": "/static/js/runtime-main.aa6e1cf3.js"
+  }
+]);
