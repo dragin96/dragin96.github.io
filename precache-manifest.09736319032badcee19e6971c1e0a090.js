f0fc8168c3796eebaf1aed2cab23ff6f470faced
dragin96 <alexxx55509@mail.ru>
2020-08-28 11:11:23 +0300

    Updates

---
+++ precache-manifest.09736319032badcee19e6971c1e0a090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0311552f9e7ffc436936f8c1ce99f43d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7a54f138dc7c0ecb586a",
+    "url": "/react-gh-pages/static/css/main.2c891d92.chunk.css"
+  },
+  {
+    "revision": "b35b5e16b8d2fe070673",
+    "url": "/react-gh-pages/static/js/2.3d4a6a32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-gh-pages/static/js/2.3d4a6a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a54f138dc7c0ecb586a",
+    "url": "/react-gh-pages/static/js/main.cc7b213d.chunk.js"
+  },
+  {
+    "revision": "4ccb60d73cf9f1358a60",
+    "url": "/react-gh-pages/static/js/runtime-main.587ef550.js"
+  }
+]);
